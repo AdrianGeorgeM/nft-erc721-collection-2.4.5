7ea802b681560c0926f2bb8dc047392685ea1b52
Adriano <62570164+AdrianGeorgeM@users.noreply.github.com>
2023-05-09 20:37:47 +0100

    Update Hidden Metadata

--- smart-contract/config/CollectionConfig.ts
+++ smart-contract/config/CollectionConfig.ts
@@ -4,32 +4,32 @@ import * as Marketplaces from '../lib/Marketplaces';
 import whitelistAddresses from './whitelist.json';
 
 const CollectionConfig: CollectionConfigInterface = {
-  testnet: Networks.ethereumTestnet,
-  mainnet: Networks.ethereumMainnet,
-  // The contract name can be updated using the following command:
-  // yarn rename-contract NEW_CONTRACT_NAME
-  // Please DO NOT change it manually!
-  contractName: 'VeryCoolApesAA',
-  tokenName: 'My NFT Token',
-  tokenSymbol: 'MNT',
-  hiddenMetadataUri: 'ipfs://__CID__/hidden.json',
-  maxSupply: 10000,
-  whitelistSale: {
-    price: 0.05,
-    maxMintAmountPerTx: 1,
-  },
-  preSale: {
-    price: 0.07,
-    maxMintAmountPerTx: 2,
-  },
-  publicSale: {
-    price: 0.09,
-    maxMintAmountPerTx: 5,
-  },
-  contractAddress: null,
-  marketplaceIdentifier: 'my-nft-token',
-  marketplaceConfig: Marketplaces.openSea,
-  whitelistAddresses,
+	testnet: Networks.ethereumTestnet,
+	mainnet: Networks.ethereumMainnet,
+	// The contract name can be updated using the following command:
+	// yarn rename-contract NEW_CONTRACT_NAME
+	// Please DO NOT change it manually!
+	contractName: 'VeryCoolApesAA',
+	tokenName: 'Very Cool Apes AA',
+	tokenSymbol: 'VCAAA',
+	hiddenMetadataUri: 'ipfs://QmP2kZGjQS6x133GnVzMowoLa6yEkTeddcdxC1RUXZZ9gy/hidden.json',
+	maxSupply: 20,
+	whitelistSale: {
+		price: 0.05,
+		maxMintAmountPerTx: 1,
+	},
+	preSale: {
+		price: 0.07,
+		maxMintAmountPerTx: 2,
+	},
+	publicSale: {
+		price: 0.09,
+		maxMintAmountPerTx: 5,
+	},
+	contractAddress: null,
+	marketplaceIdentifier: 'my-nft-token',
+	marketplaceConfig: Marketplaces.openSea,
+	whitelistAddresses,
 };
 
 export default CollectionConfig;
